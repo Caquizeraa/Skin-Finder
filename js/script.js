52fb19a29962648a29955b82a82be70561a5cf04
Caquizeraa <isacgcunha@gmail.com>
2024-07-25 14:02:02 -0300

    Resolvendo erro de Cors

--- js/script.js
+++ js/script.js
@@ -4,9 +4,8 @@ const input = document.querySelector('.input_search');
 const button = document.querySelector('.download');
 
 const fetchName = async (name) =>{
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const url = `https://api.mojang.com/users/profiles/minecraft/${name}`;
-    const apiResponse = await fetch(proxy+url);
+    const url = `https://api.ashcon.app/mojang/v2/user/${name}`;
+    const apiResponse = await fetch(url);
     const data = await apiResponse.json()
     if(apiResponse.status==200){
         return data;
@@ -17,7 +16,7 @@ const fetchSkin = async (name) =>{
     const nameData = await fetchName(name);
     if (nameData){
         skinImg.style.display = 'block';
-        const id = nameData.id;
+        const id = nameData.uuid;
         const idFinal = id.slice(0,8) + '-' + id.slice(8,12) + '-' + id.slice(12,16) + '-' + id.slice(16,20) + '-' + id.slice(20);
         const skinDw =  await fetch(`https://crafatar.com/skins/${idFinal}`);
         console.log(skinDw.url);
